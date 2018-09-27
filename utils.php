927656fb7797e40453e0c3dcf3395f8e229e7ece
Axel Rindle <axel.rindle@gmx.de>
2018-09-27 21:53:42 +0200

    Update utils.php

--- utils.php
+++ utils.php
@@ -1,11 +1,17 @@
 <?php
 
+/**
+ * @see https://stackoverflow.com/a/834355/5423625
+ */
 function str_starts_with($haystack, $needle)
 {
     $length = strlen($needle);
     return (substr($haystack, 0, $length) === $needle);
 }
 
+/**
+ * @see https://stackoverflow.com/a/834355/5423625
+ */
 function str_ends_with($haystack, $needle)
 {
     $length = strlen($needle);
